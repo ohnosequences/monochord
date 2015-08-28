6b6bd971f7438cbce1f0ff7167cf1fa6c72ed270
M42 <mromang08@gmail.com>
2015-08-28 18:47:41 +0200

    Updated definitions for Graph and MonGraph Allowing an empty graph.

--- graph.elm
+++ graph.elm
@@ -1,36 +1,17 @@
 module Graph where
 
-type alias Graph v e = {
-    edges    : List e,
-    vertices : List v,
-    source : e -> v,
-    target : e -> v
-  }
+type alias Graph v e = 
+    { edges : List e
+    , vertices : List v
+    , source : e -> Maybe v
+    , target : e -> Maybe v
+    }
 
-
--- Example: complete graphs
-complete : Int -> Graph Int (Int,Int) 
-complete n = {
-    edges = List.concatMap (\f -> List.map f [1..n]) (List.map (,) [1..n]),
-    vertices = [1..n],
-    source = fst,
-    target = snd
-  }
-
--- Example: the type graph for Open Graphs
-type TypeGraphVertices = V | E
-type TypeGraphEdges    = VtoE | EtoV | EtoE
-
-typegraph : Graph TypeGraphVertices TypeGraphEdges
-typegraph = {
-    edges = [VtoE,EtoV,EtoE],
-    vertices = [V,E],
-    source ed = case ed of
-                    VtoE -> V
-                    EtoV -> E
-                    EtoE -> E,                
-    target ed = case ed of
-                    VtoE -> E
-                    EtoV -> V
-                    EtoE -> E
-  }
+-- Empty graph
+emptygraph : Graph v e 
+emptygraph =
+    { edges = []
+    , vetices = []
+    , source = Nothing
+    , target = Nothing 
+    }
--- mongraph.elm
+++ mongraph.elm
@@ -6,3 +6,12 @@ type alias MonGraph v e = {
     source : e -> List v,
     target : e -> List v
   }
+
+-- Empty graph
+emptygraph : Graph v e 
+emptygraph =
+    { edges = []
+    , vetices = []
+    , source e = []
+    , target e = [] 
+    }
