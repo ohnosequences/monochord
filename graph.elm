95742825d344649c9d525c0995f2213d3a2bf9d3
M42 <mromang08@gmail.com>
2015-08-28 12:38:11 +0200

    Example graphs A n-complete graph and the type graph

--- graph.elm
+++ graph.elm
@@ -6,3 +6,31 @@ type alias Graph v e = {
     source : e -> v,
     target : e -> v
   }
+
+
+-- Example: complete graphs
+complete : Int -> Graph Int (Int,Int) 
+complete n = {
+    edges = List.concatMap (\f -> List.map f [1..n]) (List.map (,) [1..n]),
+    vertices = [1..n],
+    source = fst,
+    target = snd
+  }
+
+-- Example: the type graph for Open Graphs
+type TypeGraphVertices = V | E
+type TypeGraphEdges    = VtoE | EtoV | EtoE
+
+typegraph : Graph TypeGraphVertices TypeGraphEdges
+typegraph = {
+    edges = [VtoE,EtoV,EtoE],
+    vertices = [V,E],
+    source ed = case ed of
+                    VtoE -> V
+                    EtoV -> E
+                    EtoE -> E,                
+    target ed = case ed of
+                    VtoE -> E
+                    EtoV -> V
+                    EtoE -> E
+  }
