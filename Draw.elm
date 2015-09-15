c0a333b4cc01f23c1d9a1bd69546ec6f75de8caf
M42 <mromang08@gmail.com>
2015-09-16 00:10:50 +0200

    Update elm-package

--- Draw.elm
+++ Draw.elm
@@ -5,9 +5,15 @@ import Diagrams.Align exposing (..)
 import Diagrams.FillStroke exposing (..)
 import Diagrams.FullWindow exposing (..)
 import Diagrams.Debug exposing (..)
-import Color
+import Color exposing (..)
 import Signal
 import Time
 import Window
 
-main = fullWindowMain <| eqTriangle 10 (justSolidFill Color.blue)
+-- drawGraph : Graph -> Diagram t a 
+point : Diagram t a
+point = circle 0.5 (justSolidFill black)
+
+    
+
+main = fullWindowMain <| ngon 6 100 (justSolidFill black)
