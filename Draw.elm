de574e503ee615f4d45e95820dd315c567523e82
M42 <mromang08@gmail.com>
2015-09-21 11:47:08 +0200

    Draw an identified node

--- Draw.elm
+++ Draw.elm
@@ -2,18 +2,27 @@ module Draw where
 
 import Diagrams.Core exposing (..)
 import Diagrams.Align exposing (..)
+import Diagrams.Pad exposing (..)
+import Diagrams.Geom exposing (..)
+import Diagrams.Bezier exposing (..)
 import Diagrams.FillStroke exposing (..)
-import Diagrams.FullWindow exposing (..)
+import Diagrams.Actions exposing (..)
+import Diagrams.Query exposing (..)
 import Diagrams.Debug exposing (..)
-import Color exposing (..)
+import Diagrams.FullWindow as DFW
+import Text
+import Color
 import Signal
 import Time
-import Window
+import Window exposing (..)
+
+type alias NodeId = String 
 
 -- drawGraph : Graph -> Diagram t a 
-point : Diagram t a
-point = circle 0.5 (justSolidFill black)
+viewNode : NodeId -> Diagram t a
+viewNode ids = let
+        textid = text ids Text.defaultStyle
+        node = circle 10 (fillAndStroke (Solid Color.blue) defaultStroke)
+    in above textid node
 
-    
-
-main = fullWindowMain <| ngon 6 100 (justSolidFill black)
+main = DFW.fullWindowMain <| viewNode "foo"
