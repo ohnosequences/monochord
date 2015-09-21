d7c79641ef75a9f16e1cc741221d579bd3901441
M42 <mromang08@gmail.com>
2015-09-21 15:59:04 +0200

    Draw edge

--- Draw.elm
+++ Draw.elm
@@ -10,6 +10,8 @@ import Diagrams.Actions exposing (..)
 import Diagrams.Query exposing (..)
 import Diagrams.Debug exposing (..)
 import Diagrams.FullWindow as DFW
+import Graphics.Collage as C
+import Dict
 import Text
 import Color
 import Signal
@@ -17,12 +19,33 @@ import Time
 import Window exposing (..)
 
 type alias NodeId = String 
+type alias DrawEdge = { from : NodeId, to : NodeId }
+type alias DrawGraph = { nodes : Dict.Dict NodeId Point
+                       , edges : List DrawEdge
+                       }
 
--- drawGraph : Graph -> Diagram t a 
+-- Shows a circle representing a node, with a given ID.
 viewNode : NodeId -> Diagram t a
 viewNode ids = let
         textid = text ids Text.defaultStyle
         node = circle 10 (fillAndStroke (Solid Color.blue) defaultStroke)
     in above textid node
+
+viewEdge : DrawGraph -> DrawEdge -> Diagram t a
+viewEdge graph edge =
+    let from = case Dict.get edge.from graph.nodes of 
+                    Just a -> a
+                    Nothing -> error
+        to   = Dict.get edge.to graph.nodes
+    in viewGenericEdge from to
+   
+viewGenericEdge : Point -> Point -> Diagram t a 
+viewGenericEdge fromCoords toCoords =
+      let (fcx, fcy) = fromCoords
+          (tcx, tcy) = toCoords
+          cpSpacing = 100
+      in bezier fromCoords (fcx+cpSpacing, fcy)
+                (tcx-cpSpacing, tcy) toCoords
+                C.defaultLine
 
 main = DFW.fullWindowMain <| viewNode "foo"
